8fbeb91e0aeea62706cfd30086830024082447fc
mohaali77 <mzr.mohand@gmail.com>
2023-08-14 23:47:41 +0200

    modif

---
+++ sportsee/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import logo from '../images/logo.png'
+
+export function Header() {
+    return <>
+        <header>
+            <img src="" alt="" />
+            <nav>
+                <div>Accueil</div>
+                <div>Profil</div>
+                <div>Réglage</div>
+                <div>Communauté</div>
+            </nav>
+        </header>
+    </>
+
+}
