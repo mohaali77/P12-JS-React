590eea46e61cbf908499161dd55b988599cd4124
mohaali77 <mzr.mohand@gmail.com>
2023-08-15 14:03:50 +0200

    modif

---
+++ sportsee/src/components/MacroNutrients.jsx
@@ -0,0 +1,11 @@
+export function MacroNutrients() {
+    return <>
+        <div className="macroNutrients">
+            <img src="" alt="" />
+            <div>
+                <div className="values">1,930kCal</div>
+                <div className="calories">Calories</div>
+            </div>
+        </div>
+    </>
+}
