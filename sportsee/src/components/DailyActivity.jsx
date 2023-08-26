e281426b598e8aac0ebaf0547199403e15b26e31
mohaali77 <mzr.mohand@gmail.com>
2023-08-26 22:30:07 +0200

    Update DailyActivity.jsx

--- sportsee/src/components/DailyActivity.jsx
+++ sportsee/src/components/DailyActivity.jsx
@@ -5,7 +5,6 @@ import {
     YAxis,
     CartesianGrid,
     Tooltip,
-    Legend,
     ResponsiveContainer
 } from "recharts";
 
@@ -58,21 +57,20 @@ export function DailyActivity() {
 
     return <>
         <section className="dailyActivity_container">
-            <ResponsiveContainer width="100%" height="100%">
+            <div>
+                <div>Activité quotidienne</div>
+                <div><i class="fa-solid fa-circle"></i>Poids (kg)</div>
+                <div><i class="fa-solid fa-circle"></i>Calories brûlées (kCal)</div>
+            </div>
+            <ResponsiveContainer width="100%" height="80%">
                 <BarChart
                     data={data}
-                    margin={{
-                        top: 5,
-                        right: 20,
-                        left: 20,
-                        bottom: 5
-                    }}
+
                 >
                     <CartesianGrid strokeDasharray="2.5 2.5" vertical={false} />
-                    <XAxis dataKey="name" />
-                    <YAxis YAxisId="right" tickCount={3} orientation="right" />
+                    <XAxis dataKey="name" tickLine={false} tickMargin={20} />
+                    <YAxis axisLine={false} tickLine={false} YAxisId="right" tickCount={3} tickMargin={40} orientation="right" />
                     <Tooltip />
-                    <Legend verticalAlign="top" />
                     <Bar barSize={7} dataKey="pv" radius={[5, 5, 0, 0]} fill="#282D30" />
                     <Bar barSize={7} dataKey="uv" radius={[5, 5, 0, 0]} fill="#E60000" />
                 </BarChart>
