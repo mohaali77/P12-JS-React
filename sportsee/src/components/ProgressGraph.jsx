0782eae7f80748607b3928b73b0d58e92aafab6c
mohaali77 <mzr.mohand@gmail.com>
2023-08-28 00:37:24 +0200

    modif

--- sportsee/src/components/ProgressGraph.jsx
+++ sportsee/src/components/ProgressGraph.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { PieChart, Pie, Cell, ResponsiveContainer } from "recharts";
+import { PieChart, Pie, Cell } from "recharts";
 
 import "../styles/ProgressGraph/ProgressGraph.css"; // Assurez-vous d'avoir les styles CSS appropriés
 
@@ -14,19 +14,22 @@ export function ProgressGraph() {
 
     return (
         <section className="progressGraph_container">
+            <div className="percentage">
+                {percentage}% <br />
+                <span>de votre <br /> objectif</span>
+            </div>
+            <div className="score">
+                Score
+            </div>
             <div className="percentage-circle-chart">
-                <div className="percentage">
-                    {percentage}% <br />
-                    <span>de votre <br /> objectif</span>
-                </div>
                 <PieChart width={200} height={200}  >
                     <Pie
                         data={data}
                         dataKey="value"
                         cx="50%"
                         cy="50%"
-                        innerRadius={40}
-                        outerRadius={48}
+                        innerRadius={70}
+                        outerRadius={80}
                         startAngle={90}
                         endAngle={-270}
                         paddingAngle={0}
@@ -38,5 +41,6 @@ export function ProgressGraph() {
                 </PieChart>
             </div>
         </section>
+        /** */
     );
 }
