5d7b86563f13150111656b4feff48009f36e8185
mohaali77 <mzr.mohand@gmail.com>
2023-09-04 01:28:40 +0200

    Update App.js

--- sportsee/src/App.js
+++ sportsee/src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { ProfilePage } from "./pages/ProfilePage";
-import HomePage from "./pages/HomePage";
+import { HomePage } from "./pages/HomePage";
+import { ErrorPage } from "./pages/HomePage";
 
 function App() {
   return <Routes>
     <Route path="/" element={<HomePage />} />
     <Route path="/user/:id" element={<ProfilePage />} />
+    <Route path="*" element={<ErrorPage />} />
   </Routes>
 }
 
