7889a397893d98d450ba21c9cd5caeb191d04b02
mohaali77 <mzr.mohand@gmail.com>
2023-09-04 01:29:35 +0200

    Update ErrorPage.jsx

---
+++ sportsee/src/pages/ErrorPage.jsx
@@ -0,0 +1,22 @@
+import { NavLink, Link } from "react-router-dom";
+import { USER_MAIN_DATA } from '../data/mock.js'
+
+export default function HomePage() {
+
+    const data = USER_MAIN_DATA
+
+    return <>
+        <ul className='homeUsers'>
+            {
+                data.map(data =>
+                    <Link to={`/user/${data.id}`} key={data.id}>
+                        <li>
+                            User : {data.userInfos.firstName}
+                        </li>
+                    </Link>
+                )}
+        </ul>
+    </>;
+};
+
+
