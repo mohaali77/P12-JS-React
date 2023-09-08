df2d9024e7d072a884783035dff67ad2242e9f39
mohaali77 <mzr.mohand@gmail.com>
2023-09-08 23:54:16 +0200

    modif

--- sportsee/src/components/DurationGraph/DurationGraph.jsx
+++ sportsee/src/components/DurationGraph/DurationGraph.jsx
@@ -1,5 +1,5 @@
 import './style/DurationGraph.css'
-
+import { useParams } from 'react-router-dom';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const data = [
@@ -47,23 +47,27 @@ const data = [
     },
 ];
 
-export function DurationGraph() {
+export function DurationGraph(mockData) {
+
+    const { id } = useParams()
+    const dataUserId = mockData.data.find(obj => obj.userId === Number(id));
+    console.log(dataUserId.sessions);
 
     return <>
         <section className="durationGraph_container">
             <div className="durationGraph_part"></div>
             <div className="durationSessions">Durée moyenne des <br /> sessions</div>
             <ResponsiveContainer width="100%" height="55%" >
-                <LineChart width={300} height={100} data={data}>
+                <LineChart width={300} height={100} data={dataUserId.sessions}>
                     <Line
                         type="monotone"
-                        dataKey="pv"
+                        dataKey="sessionLength"
                         stroke="#FFFF"
                         strokeWidth={2}
                         dot={false} //supprimes les points du graph
 
                     />
-                    <Tooltip dataKey="pv" />
+                    <Tooltip dataKey="day" />
                 </LineChart >
             </ResponsiveContainer >
             <ul className="daysWeek">
