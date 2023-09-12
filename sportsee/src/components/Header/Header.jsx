cda7416870378efc26a51bc2fcd3d70fabd674cd
mohaali77 <mzr.mohand@gmail.com>
2023-09-12 22:00:09 +0200

    modif

--- sportsee/src/components/Header/Header.jsx
+++ sportsee/src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import './style/Header.css'
+import { Link } from 'react-router-dom'
 
 import logo from '../../images/logo.png'
 
@@ -7,10 +8,10 @@ export function Header() {
         <header>
             <img src={logo} alt="Logo du site web SportSee" />
             <nav>
-                <div>Accueil</div>
-                <div>Profil</div>
-                <div>Réglage</div>
-                <div>Communauté</div>
+                <Link to={'/'} ><div>Accueil</div></Link>
+                <Link to={'/'} ><div>Profil</div></Link>
+                <Link to={'/'} ><div>Réglage</div></Link>
+                <Link to={'/'} ><div>Communauté</div></Link>
             </nav>
         </header>
     </>
