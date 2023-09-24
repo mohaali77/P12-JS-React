b58ef2a5eeb8ec04bdcf9b8521fb34254eb8adf8
mohaali77 <mzr.mohand@gmail.com>
2023-09-24 19:09:32 +0200

    Update HomePage.jsx

--- sportsee/src/pages/HomePage/HomePage.jsx
+++ sportsee/src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,7 @@
-
-
+//Import Fonctionnalités, Hook, Bibliothèque...
 import { Link } from "react-router-dom";
-// Import Données
+
+// Import Données Mock
 import { USER_MAIN_DATA } from '../../data/mock.js'
 
 // Import CSS
@@ -9,11 +9,13 @@ import './style/HomePage.css'
 
 export default function HomePage() {
 
+    // On récupère les données du mock dans une constante
     const data = USER_MAIN_DATA
 
     return <>
         <ul className='users'>
             {
+                // Pour chaque utilisateur, on va créer un lien qui le redirigera vers une page avec son id correspondant. 
                 data.map((data, index) =>
                     <Link to={`/user/${data.id}`} key={data.id}>
                         <li>
