a778ad9a1a7db1c72a76b6b5adde00011026b44c
mohaali77 <mzr.mohand@gmail.com>
2023-09-25 21:40:56 +0200

    Update ProgressGraph.jsx

--- sportsee/src/components/ProgressGraph/ProgressGraph.jsx
+++ sportsee/src/components/ProgressGraph/ProgressGraph.jsx
@@ -1,53 +1,15 @@
-// Import Données API
-import { getData } from '../../data/service'
-
-//Import Fonctionnalités, Hook, Bibliothèque...
-import { useParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-
-//Import CSS
+// Import CSS
 import './style/ProgressGraph.css'
 
 // Import des composants "recharts" pour construire le graphique
 import { PieChart, Pie, Cell } from "recharts";
 
-export function ProgressGraph(mockData) {
+export function ProgressGraph({ todayScore }) {
 
-    // On récupère l'id présent dans l'URL
-    const { id } = useParams()
-
-    // Récupération données via API ou Mock si l'API n'est pas chargé ou qu'il y a une erreur
-    const [data, setData] = useState([])
-
-    useEffect(() => {
-        async function getDataLoad() {
-            try {
-                const fetchedData = await getData(id);
-                if (fetchedData) {
-                    setData(fetchedData.data);
-                } else {
-                    setData(mockData.data.find(obj => obj.id === Number(id)));
-                }
-            } catch (error) {
-                setData(mockData.data.find(obj => obj.id === Number(id)));
-                console.log(error);
-            }
-        }
-        getDataLoad();
-    }, [id]);
-
-    if (data) {
-        // console.log(todayScore);
-    }
-
-
-    console.log(data);
     let percentage
 
-    if (data.todayScore) {
-        percentage = data.todayScore * 100;
-    } else if (data.score) {
-        percentage = data.score * 100;
+    if (todayScore) {
+        percentage = todayScore * 100;
     }
 
     const pieChartData = [
