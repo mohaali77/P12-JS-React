5e051f8e7ec205af89eceb2362cc38df995b7122
mohaali77 <mzr.mohand@gmail.com>
2023-09-25 19:10:42 +0200

    Update DailyActivity.jsx

--- sportsee/src/components/DailyActivity/DailyActivity.jsx
+++ sportsee/src/components/DailyActivity/DailyActivity.jsx
@@ -24,49 +24,39 @@ export function DailyActivity(mockData) {
     class UserSessions {
         constructor(data) {
 
-            if (!Array.isArray(data)) {
-                throw new Error('Les données doivent être un tableau.');
+            if (typeof data !== 'object') {
+                throw new Error('Chaque élément du tableau doit être un objet.');
             }
 
-            this.sessions = data.map(data => {
-                if (typeof data !== 'object') {
-                    throw new Error('Chaque élément du tableau doit être un objet.');
-                }
+            if (!data.hasOwnProperty('day') || !data.hasOwnProperty('kilogram') || !data.hasOwnProperty('calories')) {
+                throw new Error('Les données doivent contenir les propriétés "day", "kilogram" et "calories".');
+            }
 
-                if (typeof data !== 'object') {
-                    throw new Error('Les données doivent être un objet.');
-                }
+            if (typeof data.day !== 'string') {
+                throw new Error('La donnée "day" doit être un string');
+            }
 
-                if (!data.hasOwnProperty('day') || !data.hasOwnProperty('kilogram') || !data.hasOwnProperty('calories')) {
-                    throw new Error('Les données doivent contenir les propriétés "day", "kilogram" et "calories".');
-                }
+            if (typeof data.kilogram !== 'number') {
+                throw new Error('La donnée "kilogram" doit être un nombre');
+            }
 
-                if (typeof data.day !== 'string') {
-                    throw new Error('La donnée "day" doit être un string');
-                }
+            if (typeof data.calories !== 'number') {
+                throw new Error('La donnée "calories" doit être un nombre');
+            }
 
-                if (typeof data.kilogram !== 'number') {
-                    throw new Error('La donnée "kilogram" doit être un nombre');
-                }
+            this.day = data.day
+            this.kilogram = data.kilogram
+            this.calories = data.calories
 
-                if (typeof data.calories !== 'number') {
-                    throw new Error('La donnée "calories" doit être un nombre');
-                }
-            })
 
-            this.day = data.day;
-            this.kilogram = data.kilogram;
-            this.calories = data.calories;
         }
     }
 
-    let newUserSessions = new UserSessions([{
+    let newUserSessions = new UserSessions({
         day: ' ',
         kilogram: 0,
         calories: 0
-    }])
-
-    console.log(newUserSessions);
+    })
 
     const [data, setData] = useState([])
 
@@ -87,15 +77,16 @@ export function DailyActivity(mockData) {
         getDataLoad();
     }, [id]);
 
-    // On crée une nouvelle instance de la classe User qu'on mettra à jour avec les données du mock ou de l'API
-    let sessionList
-
     if (data && data.sessions) {
-        //console.log(data);
-        //console.log(data.sessions);
-        //console.log(data.session);
-        sessionList = data.sessions.map(data => new UserSessions(data.day, data.kilogram, data.calories));
+        newUserSessions = data.sessions.map(data => new UserSessions({
+            day: data.day,
+            kilogram: data.kilogram,
+            calories: data.calories
+        }
+        ));
     }
+
+    console.log(newUserSessions);
 
     return <>
         <section className="dailyActivity_container">
@@ -112,7 +103,7 @@ export function DailyActivity(mockData) {
                     right: 30,
                     left: 30,
                     bottom: 5
-                }} data={data.sessions}>
+                }} data={newUserSessions}>
                     <CartesianGrid strokeDasharray="2.5 2.5" vertical={false} />
                     <XAxis dataKey="day" tickLine={false} tickMargin={17} tickFormatter={formatXAxis} />
                     <YAxis axisLine={false} tickLine={false} YAxisId="right" tickCount={3} tickMargin={30} orientation="right" />
